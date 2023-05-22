fbd1687b425772d4b5f51d08e068cb4c9708d4da
vishal150520 <vishalkumar150520@gmail.com>
2023-05-22 12:30:22 +0530

    Session in c#

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateSession()
+        {
+            this.HttpContext.Session.SetString("sessionkey", "Vishal");
+            return RedirectToAction("GetSession");
+        }
+        public IActionResult GetSession()
+        {
+            return View();
+        }
+    }
+    
+}
